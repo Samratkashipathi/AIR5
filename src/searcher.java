72c0f008945f6aadafbd989cf2d5b8771250df3b
Samrat <samratkashipathi1@gmail.com>
2017-11-20 15:40:17 +0530

    Final

--- src/searcher.java
+++ src/searcher.java
@@ -11,6 +11,8 @@ import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MultiPhraseQuery;
+import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
@@ -35,11 +37,13 @@ public class searcher
         switch(type) {
         case 1:
         case 2:
-        case 4:
         case 5:
         case 6:	TopDocs TopFoundDocs = searchByType2(toFind, searcher);
         		displayTopResults(TopFoundDocs,searcher);
         		break;
+        case 4:	TopDocs TopPhraseDocs = searchPhrase(toFind, searcher);
+				displayTopResults(TopPhraseDocs,searcher);
+				break;
         case 3: TopDocs TopFuzzyDocs = fuzzySearchTest(toFind, searcher);
 				displayTopResults(TopFuzzyDocs,searcher);
 				break;
@@ -60,7 +64,6 @@ public class searcher
     private static TopDocs searchByType2(String toFind, IndexSearcher searcher) throws Exception
     {
     	String types[] = {"Title","Director","Cast","Genre","Notes"};
-//    	System.out.println(types+" Results:");
         MultiFieldQueryParser qp = new MultiFieldQueryParser(types, new StandardAnalyzer());
         Query query = qp.parse(toFind);
         System.out.println(query);
@@ -68,17 +71,62 @@ public class searcher
         return hits;
     }
     
+//    private static TopDocs searchPhrase(String toFind, IndexSearcher searcher) throws Exception
+//    {
+//    	String types[] = {"Title","Director","Cast","Genre","Notes"};
+//    	MultiPhraseQuery.Builder builder = new MultiPhraseQuery.Builder();
+//		builder.add(new Term("Title", "harry"));
+//		builder.add(new Term("Title", "chamber"));
+//		builder.setSlop(4);
+//		MultiPhraseQuery.Builder builder1 = new MultiPhraseQuery.Builder();
+//		builder1.add(new Term("Notes", "harry"));
+//		builder1.add(new Term("Notes", "chamber"));
+//		builder1.setSlop(4);
+//		MultiPhraseQuery pq = builder.build();
+//		MultiPhraseQuery pq1 = builder1.build();
+//        System.out.println(pq);
+//        System.out.println(pq1);
+//        TopDocs hits[] = new TopDocs[2];
+//        hits[0]=searcher.search(pq, 10);
+//        hits[1]=searcher.search(pq1, 10);
+//        TopDocs finalHits = TopDocs.merge(10, hits);
+//        return finalHits;
+//    }
+    
+    private static TopDocs searchPhrase(String toFind, IndexSearcher searcher) throws Exception
+    {
+    	String types[] = {"Title","Director","Cast","Genre","Notes"};
+    	TopDocs hits[] = new TopDocs[5];
+    	String slopeValue = toFind.split("~")[1];
+    	String words = toFind.split("~")[0];
+    	String[] terms = words.split(" ");
+    	for(int i=0;i<types.length;i++) {
+    		MultiPhraseQuery.Builder builder = new MultiPhraseQuery.Builder();
+    		for(int j=0;j<terms.length;j++) {
+    			builder.add(new Term(types[i], terms[j].toLowerCase()));
+    		}
+    		builder.setSlop(4);
+    		MultiPhraseQuery pq = builder.build();
+    		System.out.println(pq);
+    		hits[i]=searcher.search(pq, 10);
+    	}
+    	TopDocs finalHits = TopDocs.merge(10, hits);
+    	return finalHits;
+    }
+    
+    
     private static TopDocs fuzzySearchTest(String toFind,IndexSearcher searcher) throws Exception
     {
-    	//QueryParser qp = new QueryParser("Genre", new StandardAnalyzer());
     	String types[] = {"Title","Director","Cast","Genre","Notes"};
+    	TopDocs hits[] = new TopDocs[5];
     	System.out.println("Fuzzy Results");
-//    	BooleanQuery booleanQuery = new BooleanQuery();
-    	Query query = new FuzzyQuery(new Term("Genre", toFind),1);
-    	System.out.println(query);
-    	TopDocs hits = searcher.search(query, 10);
-    	//System.out.println(hits);
-    	 return hits;
+    	for(int i=0;i<types.length;i++) {
+    		Query query = new FuzzyQuery(new Term(types[i], toFind),1);
+        	System.out.println(query);
+        	hits[i] = searcher.search(query, 10);
+    	}
+    	TopDocs finalHits = TopDocs.merge(10, hits);
+        return finalHits;
     }
  
     private static IndexSearcher createSearcher() throws IOException {
@@ -91,6 +139,7 @@ public class searcher
     private static void displayTopResults(TopDocs foundDocs,IndexSearcher searcher) throws Exception{
     	System.out.println("Found : " + foundDocs.totalHits);
         
+    	System.out.println("\n");
         for (ScoreDoc sd : foundDocs.scoreDocs)
         {
             Document d = searcher.doc(sd.doc);
@@ -101,7 +150,8 @@ public class searcher
             System.out.println("Cast: "+String.format(d.get("Cast")));
             System.out.println("Genre: "+String.format(d.get("Genre")));
             System.out.println("Notes: "+String.format(d.get("Notes")));
+            System.out.println("\n");
         }
-        System.out.println("\n");
+        
     }
 }
